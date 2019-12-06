14e21a1e2c025b6bcee16ff870ddabbc7e724907
NguyenViet07 <nguyenviet.vn.07@gmail.com>
2019-12-06 09:19:24 +0700

    tao component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../block/header/header.component';
+import { MenuComponent } from '../block/menu/menu.component';
+import { ListSongComponent } from '../page/song/list-song/list-song.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ListSongComponent
   ],
   imports: [
     BrowserModule,
